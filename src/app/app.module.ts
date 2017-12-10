f3f3a0e1145b34f4e1f27b3fa441cdd2303d3608
Rodrigo Espinel <rodrigo.espinel-villalobos@insa-cvl.fr>
2017-12-10 20:06:43 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 
 
 import { AppComponent } from './app.component';
